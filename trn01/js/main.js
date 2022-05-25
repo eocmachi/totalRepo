3adb5b263b928d8ecd5d6bf051e8bf71f9ef5b0c
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-05-25 17:19:06 +0900

    Add files via upload
    2022. 05. 25

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,8 +1,18 @@
 $(function () {
-  $(".main_slider").slick({
-    arrows: false,
-    autoplay: true,
-    pauseOnFocus: true,
-    pauseOnHover: false,
-  });
+
+
+  $('.main_slider').on('init reInit afterChange', function(){
+    var here = $('.slick-current');
+    here
+      .addClass("on").siblings().removeClass("on");
+  })
+
+    $(".main_slider").slick({
+      arrows: false,
+      autoplay: true,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      fade: true,
+    });
+
 });
