561dd68977027104341a595e4ee21ee4dd5ed8d2
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-06-28 17:36:38 +0900

    Add files via upload
    2022.06.08

---
+++ portfolio-2022/js/pf-01.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('#utb').YTPlayer({
+        videoURL: 'https://youtu.be/irn6NVUDK-A', containment: '#header',
+        autoPlay: true,
+        showControls: false,
+        playOnlyIfVisible: true
+    });
+}); 
