1ca7aeb56ab7a65ec22de325847d08b8a720ae56
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-07-04 17:46:51 +0900

    Add files via upload
    2022.07.04

---
+++ portfolio-2022/js/pf-02.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('.main_slider').slick({
+        // dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+    $('.menu_slider').slick({
+        slidesToShow: 4,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+})
