e9d538bbf2904b02e949a5de1229f632edcd50e2
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-07-08 15:01:29 +0900

    Delete js directory

--- js/pf-01.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('#utb').YTPlayer({
-        videoURL: 'https://youtu.be/irn6NVUDK-A', containment: '#header',
-        autoPlay: true,
-        showControls: false
-    });
-}); 
