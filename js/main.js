508abfd07c5d039764716b617665d84b4f4bbe5e
jigooo <h00kk@naver.com>
2022-07-12 17:45:08 +0900

    2022.07.12

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $('.story-slider').slick({
+        // dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        arrows: false,
+        draggable : true,
+        cssEase: 'linear'
+      });
+
+  })
