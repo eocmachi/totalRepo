9dca6e68c7d9a1e9503e461af3039effdb3e4d4a
jigooo <h00kk@naver.com>
2022-07-13 17:48:27 +0900

    07.13

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(function () {
 
     $('.story-slider').slick({
-        // dots: true,
         infinite: true,
         speed: 500,
         fade: true,
@@ -9,6 +8,41 @@ $(function () {
         arrows: false,
         draggable : true,
         cssEase: 'linear'
+      });
+    $('.nail .right-box').slick({
+        infinite: true,
+        slidesToShow: 3,
+        speed: 500,
+        autoplay: true,
+        arrows: true,
+        draggable : true,
+        cssEase: 'linear'
+      });
+
+    $('.nail .left-box').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        arrows: false,
+        draggable : true,
+        cssEase: 'linear'
+      });
+
+      $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        asNavFor: '.slider-for',
+        focusOnSelect: true
       });
 
   })
