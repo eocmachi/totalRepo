484be1ef52da373b087e011d9c4b8ed9c2836c42
jigooo <h00kk@naver.com>
2022-07-15 14:13:15 +0900

    07.15

--- js/main.js
+++ js/main.js
@@ -1,48 +1,67 @@
 $(function () {
 
-    $('.story-slider').slick({
-        infinite: true,
-        speed: 500,
-        fade: true,
-        autoplay: true,
-        arrows: false,
-        draggable : true,
-        cssEase: 'linear'
-      });
-    $('.nail .right-box').slick({
-        infinite: true,
-        slidesToShow: 3,
-        speed: 500,
-        autoplay: true,
-        arrows: true,
-        draggable : true,
-        cssEase: 'linear'
-      });
+  $('.story-slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    arrows: false,
+    draggable: true,
+    cssEase: 'linear'
+  });
 
-    $('.nail .left-box').slick({
-        infinite: true,
-        speed: 500,
-        fade: true,
-        autoplay: true,
-        arrows: false,
-        draggable : true,
-        cssEase: 'linear'
-      });
+  $('.nail .left-box').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    arrows: false,
+    draggable: true,
+    cssEase: 'linear',
+    asNavFor: '.nail .right-box'
+  });
 
-      $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
+  $('.nail .right-box').slick({
+    infinite: true,
+    slidesToShow: 3,
+    speed: 500,
+    autoplay: true,
+    arrows: true,
+    draggable: true,
+    cssEase: 'linear',
+    asNavFor: '.nail .left-box',
 
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        asNavFor: '.slider-for',
-        focusOnSelect: true
-      });
+    prevArrow : $('.nail-prevArrow'), 
+    nextArrow : $('.nail-nextArrow'), 
 
-  })
+  });
+
+  $('.left').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.target'
+  });
+
+  $('.mid-box').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    asNavFor: '.target',
+
+    prevArrow : $('.mid-prevArrow'), 
+    nextArrow : $('.mid-nextArrow'), 
+  });
+
+  $('.right').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    asNavFor: '.target',
+    focusOnSelect: true
+  });
+
+
+
+})
