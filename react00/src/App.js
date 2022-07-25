c4cc6c73a2ab91c1ad9b4fd6a193406014864d11
jigooo <h00kk@naver.com>
2022-07-25 12:22:14 +0900

    0725

---
+++ react00/src/App.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+import Test from "./test";
+const App = () => {
+  //console.log('하하하')
+  return (
+  <>
+  <Test />    {/* 대문자로 써주는게 원칙 */}
+  <h1>ghghgh</h1>
+  <Nav />
+  <Test />
+  <Test />
+  {/* <h2>haaaaaaaaaaa</h2> */}
+  </>
+  ) 
+
+}
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,12 @@
+function Test(){
+    return (
+        <>
+        <i className="xi-bars"></i>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolor voluptas ex inventore earum minima.
+        </p>
+        </>
+    )
+}
+
+export default Test;
