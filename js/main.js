e1f4d3b5caa893ad89bda387aac37d0caa34863b
jigooo <h00kk@naver.com>
2022-08-02 17:03:47 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    var swiper = new Swiper(".archive-swiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    });
+  
+  })
+  
