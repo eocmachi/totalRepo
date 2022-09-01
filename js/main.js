419fa8a04048e9769941b9ed739a1817c96200da
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-09-02 01:58:48 +0900

    Add files via upload
    부가적인 기능들 추가하기....

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
 const main = new Swiper(".main.swiper.mySwiper", {
-    loop: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
 });
 
 const best = new Swiper(".best.swiper.mySwiper", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup : 3,
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  pagination: {
+    el: ".best.swiper-pagination",
+  },
 });
 
 const review = new Swiper(".review.swiper.mySwiper", {
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: 2.65,
-    spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 2.65,
+  spaceBetween: 30,
+  pagination: {
+    el: ".review.swiper-pagination",
+  },
+});
+
+const Header = document.querySelector("#Header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    Header.classList.add("on");
+  } else {
+    Header.classList.remove("on");
+  }
 });
