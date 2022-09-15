e29ff607e8924c3af8789387c5f50a46c5621b4d
jigooo <h00kk@naver.com>
2022-09-15 11:51:16 +0900

    0915

---
+++ src/component/Promotion.js
@@ -0,0 +1,14 @@
+import YouTube from "react-youtube"
+
+const Promotion = () => {
+    return (
+        <section className="">
+            <h2>
+                <p>건물 특성에 맞는 다양한 제품으로 고객의 요구를 충족시켜 드립니다.</p>
+                <div className="">
+                    <div></div>
+                </div>
+            </h2>
+        </section>
+    )
+}
