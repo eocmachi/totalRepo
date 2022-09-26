9a27bc6275f37b3def6d94f0ef703d6b8bf4bf37
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-09-26 17:48:28 +0900

    Add files via upload
    field.js 수정

--- carrot/src/main.js
+++ carrot/src/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 import PopUp from "./popup.js";
+import Field from "./field.js";
 
-const CARROT_SIZE = 80;
 const CARROT_COUNT = 20;
 const BUG_COUNT = 20;
 const GAME_DURATION_SEC = 20;
@@ -26,7 +26,23 @@ gameFinishBanner.setClickListener(() => {
   startGame();
 });
 
-field.addEventListener("click", onFiledClick);
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickListener(onItemClick);
+
+function onItemClick(item) {
+  if (!started) {
+    return;
+  }
+  if (item === "carrot") {
+    score++;
+    updateScoreBoard();
+    if (score === CARROT_COUNT) {
+      finishGame(true);
+    }
+  } else if (item === "bug") {
+    finishGame(false);
+  }
+}
 
 gameBtn.addEventListener("click", () => {
   if (started) {
@@ -110,26 +126,7 @@ function stopGameTimer() {
 function initGame() {
   score = 0;
   gameScore.innerText = CARROT_COUNT;
-}
-
-function onFiledClick(event) {
-  if (!started) {
-    return;
-  }
-  const target = event.target;
-  if (target.matches(".carrot")) {
-    // 당근!!
-    target.remove();
-    score++;
-    playSound(carrotSound);
-    updateScoreBoard();
-    if (score === CARROT_COUNT) {
-      finishGame(true);
-    }
-  } else if (target.matches(".bug")) {
-    // 버그ㅜㅜ
-    finishGame(false);
-  }
+  gameField.init();
 }
 
 function playSound(sound) {
@@ -147,6 +144,3 @@ function updateScoreBoard() {
 
 
 
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-}
