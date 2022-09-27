5ba2d9ae6f41cc9aab9f7ceff0af18a7ac1b74de
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-09-27 23:17:19 +0900

    Add files via upload

--- carrot/src/field.js
+++ carrot/src/field.js
@@ -1,5 +1,6 @@
 "use strict";
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
+import * as sound from "./sound.js";
+
 const CARROT_SIZE = 80;
 
 export default class Field {
@@ -17,11 +18,9 @@ export default class Field {
     this._addItem("bug", this.bugCount, "img/bug.png");
   }
 
-
   setClickListener(onItemClick) {
     this.onItemClick = onItemClick;
   }
-
 
   _addItem(className, count, imgPath) {
     const x1 = 0;
@@ -41,24 +40,18 @@ export default class Field {
     }
   }
 
-  onClick(event) {
+  onClick = (event) => {
     const target = event.target;
-  if (target.matches(".carrot")) {
-    target.remove();
-    playSound(carrotSound);
-    this.onItemClick && this.onItemClick('carrot');
-  } else if (target.matches(".bug")) {
-    this.onItemClick && this.onItemClick('bug');
-  }
-  }
+    if (target.matches(".carrot")) {
+      target.remove();
+      sound.playCarrot();
+      this.onItemClick && this.onItemClick("carrot");
+    } else if (target.matches(".bug")) {
+      this.onItemClick && this.onItemClick("bug");
+    }
+  };
 }
-
 
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
 }
