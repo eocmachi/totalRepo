15479b6bec00b81a214209b331469a7c512c93df
jigooo <h00kk@naver.com>
2022-09-28 16:10:35 +0900

    0928

---
+++ src/shop/Cart.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Cart = ({ cart }) => {
+  return (
+    <div style={{ paddingTop: "500px" }}>
+      {
+      cart.map(ca => {
+        return (
+          <ul>
+            <li>{ca.id}</li>
+            <li>{ca.itm}</li>
+          </ul>
+        );
+      })
+      }
+    </div>
+  );
+};
+
+export default Cart;
