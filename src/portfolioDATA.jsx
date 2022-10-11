3225614267a012c770e172affb5cc225b9ec20e9
jigooo <h00kk@naver.com>
2022-10-11 12:57:10 +0900

    1011

---
+++ src/portfolioDATA.jsx
@@ -0,0 +1,35 @@
+const PD = [
+  {
+    id: 1,
+    title: "pf01",
+    color: ["#333", "#333", "#333"],
+    content: "first homepage",
+    link: "jigoooo.github.io/BIFF",
+    anchor: "pf01",
+  },
+  {
+    id: 2,
+    title: "pf02",
+    color: ["#333", "#333", "#333"],
+    content: "2nd homepage",
+    link: "jigoooo.github.io/BIFF",
+    anchor: "pf02",
+  },
+  {
+    id: 3,
+    title: "pf03",
+    color: ["#333", "#333", "#333"],
+    content: "3rd homepage",
+    link: "jigoooo.github.io/BIFF",
+    anchor: "pf03",
+  },
+  {
+    id: 4,
+    title: "pf04",
+    content: "4th homepage",
+    link: "jigoooo.github.io/BIFF",
+    anchor: "pf04",
+  },
+];
+
+export default PD;
