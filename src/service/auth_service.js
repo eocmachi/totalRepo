abcfa5642bc2307872089719fc04d8e9348c7b18
이정은 <j@ijeong-eun-ui-MacBookAir.local>
2022-11-04 16:06:33 +0900

    1104

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,5 +1,10 @@
 import firebase from "firebase";
 
 class AuthService {
-    login()
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebase.auth().signInWithPopup(authProvider);
+  }
 }
+
+export default AuthService;
