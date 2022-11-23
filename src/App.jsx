2e0a35cd4d86703064114f400ca5cdf1396e0fae
jigooo <h00kk@naver.com>
2022-11-23 17:58:16 +0900

    너무 어려움...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Login from "./components/login/login";
-import styles from "./app.module.css"
+import styles from "./app.module.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Maker from "./components/maker/maker";
 
 const App = ({ authService }) => {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login authService={authService} />} />
+          <Route path="/maker" element={<Maker authService={authService} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
