d460ce81299aa664f425435aee3607c4de1c5b3c
jigooo <h00kk@naver.com>
2023-01-18 18:40:59 +0900

    0118

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const card = new Swiper(".card", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+  });
+
+const review = new Swiper(".review", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    // autoplay: true,
+    loopFillGroupWithBlank: true,
+  });
