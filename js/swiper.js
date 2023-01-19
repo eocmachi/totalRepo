f4346c22427dcecf3d0ccd02fa66d2849b82c249
jigooo <h00kk@naver.com>
2023-01-20 01:59:37 +0900

    0120

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,39 @@
+const btn = document.querySelector(".family-site");
+
+function handleBtnClick() {
+  btn.classList.toggle("on");
+}
+btn.addEventListener("click", handleBtnClick);
+
+const main = new Swiper(".main", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+  pagination: {
+    el: ".main.swiper-pagination",
+    clickable: true,
+  },
+});
+
 const card = new Swiper(".card", {
   slidesPerView: 4,
   spaceBetween: 30,
   slidesPerGroup: 4,
   loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
   loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".cure-next",
+    prevEl: ".cure-prev",
+    clickable: true,
+  },
 });
 
 const review = new Swiper(".review", {
@@ -11,14 +41,19 @@ const review = new Swiper(".review", {
   spaceBetween: 30,
   slidesPerGroup: 4,
   loop: true,
-  // autoplay: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
   loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".review-next",
+    prevEl: ".review-prev",
+    clickable: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
 });
-
-const btn = document.querySelector(".family-site");
-
-function handleBtnClick() {
-  btn.classList.toggle("on");
-}
-
-btn.addEventListener("click", handleBtnClick);
