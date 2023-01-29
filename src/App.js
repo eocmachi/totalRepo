d754f443312a0ed63277558521016cd8ff3023c5
jigooo <h00kk@naver.com>
2023-01-29 22:27:04 +0900

    0129

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Carts from "./components/Carts/Carts";
+import New from "./components/NewProduct/NewProduct";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
+import { AuthContextProvider } from "./context/AuthContext";
+import AllProducts from "./components/AllProducts/AllProducts";
+import Main from "./components/Main/Main";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Footer from "./components/Footer/Footer";
+import "./css/app.scss";
+import BrandIssue from "./components/BrandIssue/BrandIssue";
+import Sunglasses from "./Pages/Sunglasses";
+import Glasses from "./Pages/Glasses";
 
-function App() {
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/sunglasses" element={<Sunglasses />} />
+          <Route path="/glasses" element={<Glasses />} />
+          <Route path="/carts" element={<Carts />} />
+          <Route path="/brandIssue" element={<BrandIssue />} />
+          <Route
+            path="/products/new"
+            element={
+              <ProtectedRoute requireAdmin>
+                <New />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
